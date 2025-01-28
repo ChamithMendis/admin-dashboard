fccb06675fbbd552cd6f70f2e557099efa68a881
Chamith Mendis <m.chamith@directfn.com>
2025-01-28 17:29:15 +0530

    initial routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,17 @@ import { DefaultLayoutComponent } from './default-layout/default-layout.componen
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
     component: DefaultLayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./pages/home/routes').then((m) => m.routes),
+      },
+    ],
   },
 ];
