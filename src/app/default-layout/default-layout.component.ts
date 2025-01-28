30ed144048de43c8f5f8b1c112cc3b47e3c1e7a7
Chamith Mendis <m.chamith@directfn.com>
2025-01-28 17:13:42 +0530

    created initial draft for sidebar

--- src/app/default-layout/default-layout.component.ts
+++ src/app/default-layout/default-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-default-layout',
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './default-layout.component.html',
-  styleUrl: './default-layout.component.scss'
+  styleUrl: './default-layout.component.scss',
 })
-export class DefaultLayoutComponent {
-
-}
+export class DefaultLayoutComponent {}
--- src/app/default-layout/sidebar/sidebar.component.ts
+++ src/app/default-layout/sidebar/sidebar.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    RouterOutlet,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile = true;
+  isCollapsed = true;
 
+  constructor(private observer: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if (screenSize.matches) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+  // put this in header
+  toggleMenu() {
+    if (this.isMobile) {
+      this.sidenav.toggle();
+      this.isCollapsed = false; // On mobile, the menu can never be collapsed
+    } else {
+      this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 }
