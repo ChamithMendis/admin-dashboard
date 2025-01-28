9e77f75b82fa69a82f140308fd7be214ec54c47d
Chamith Mendis <m.chamith@directfn.com>
2025-01-28 23:38:51 +0530

    notification badge setup

--- src/app/default-layout/header/header.component.ts
+++ src/app/default-layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,8 +7,29 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatBadgeModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  notifications: Array<Object> = [];
+  noOfNotifications: number | null = null;
+
+  public onNotificationIconClick(): void {
+    this.onNotificationReceive();
+  }
+
+  public onNotificationReceive(): void {
+    this.notifications.push({
+      message: 'This is a notification',
+    });
+
+    this.noOfNotifications = this.notifications.length;
+  }
+}
