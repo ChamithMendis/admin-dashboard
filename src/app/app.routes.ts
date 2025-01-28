a0dfff589284d11ecf3e85f34f9dbe9d0a8632a5
Chamith Mendis <m.chamith@directfn.com>
2025-01-28 15:57:23 +0530

    initial setup for header, footer and sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './default-layout/default-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+  },
+];
