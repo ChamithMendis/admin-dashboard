8a396dc00964f8afbd57f97e3b22c7e5b91ba7ab
Chamith Mendis <m.chamith@directfn.com>
2025-02-01 09:25:07 +0530

    notification menu item dropdown

--- src/app/default-layout/header/header.component.ts
+++ src/app/default-layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -13,23 +14,53 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatIconModule,
     MatTooltipModule,
     MatBadgeModule,
+    MatMenuModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  notifications: Array<Object> = [];
+  notifications: Array<NotificationI> = [];
   noOfNotifications: number | null = null;
 
-  public onNotificationIconClick(): void {
+  constructor() {
     this.onNotificationReceive();
   }
 
+  public onNotificationIconClick(): void {}
+
   public onNotificationReceive(): void {
     this.notifications.push({
-      message: 'This is a notification',
+      id: 1,
+      message: 'Welcome to the One Piece Dashboard!',
+      read: false,
+    });
+    this.notifications.push({
+      id: 2,
+      message: 'You have successfully singed in!',
+      read: false,
+    });
+    this.notifications.push({
+      id: 3,
+      message: 'Lets create your profile first',
+      read: false,
     });
 
     this.noOfNotifications = this.notifications.length;
   }
+
+  public onNotificationItemClick(notification: NotificationI): void {
+    const notificationItem = this.notifications.find(
+      (item) => item.id === notification.id
+    );
+    if (notificationItem) {
+      notificationItem.read = true;
+    }
+  }
+}
+
+interface NotificationI {
+  id: number;
+  message: string;
+  read: boolean;
 }
