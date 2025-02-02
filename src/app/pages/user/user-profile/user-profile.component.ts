7b2f26b2c3a6ec5234f6c934454b8a0d58115dc5
Chamith Mendis <m.chamith@directfn.com>
2025-02-02 11:15:01 +0530

    form section

--- src/app/pages/user/user-profile/user-profile.component.ts
+++ src/app/pages/user/user-profile/user-profile.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrl: './user-profile.component.scss',
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  profileForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.profileForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      secondName: [''],
+      lastName: ['', Validators.required],
+      age: [''],
+      dob: [''],
+      email: ['', [Validators.required, Validators.email]],
+      occupation: [''],
+      maritalStatus: [''],
+      userName: [''],
+      password: ['', Validators.minLength(6)],
+    });
+  }
+
+  ngOnInit() {}
+
+  public onSubmit(): void {}
 }
